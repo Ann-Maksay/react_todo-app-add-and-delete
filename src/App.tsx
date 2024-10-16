37f4d665f8871aa145f0fc18d88e3af1a994c13e
Anna Maksai <annamaksai99@gmail.com>
2024-10-16 10:21:26 +0300

    feat: load todo by user

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
-import { UserWarning } from './UserWarning';
-
-const USER_ID = 0;
+import React, { useEffect, useMemo, useState } from 'react';
+import { Header, Footer, Notification, TodoList } from './components';
+import { getTodos } from './api/todos';
+import { FilterOption, Todo, Error } from './types';
 
 export const App: React.FC = () => {
-  if (!USER_ID) {
-    return <UserWarning />;
-  }
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [error, setError] = useState<Error>(Error.DEFAULT);
+  const [filterOption, setFilterOption] = useState<FilterOption>(
+    FilterOption.all,
+  );
+
+  const todosAmount = useMemo(() => todos.length, [todos]);
+  const uncompletedTodosAmount = useMemo(
+    () => todos.reduce((amount, todo) => amount + (todo.completed ? 0 : 1), 0),
+    [todos],
+  );
+
+  const handleResetError = () => setError(Error.DEFAULT);
+
+  const handleError = (message: Error) => {
+    setError(message);
+
+    setTimeout(handleResetError, 3000);
+  };
+
+  useEffect(() => {
+    handleResetError();
+
+    getTodos()
+      .then(currentTodos => {
+        setTodos(currentTodos);
+      })
+      .catch(() => {
+        handleError(Error.LOADING_TODOS);
+      });
+  }, []);
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header
+          todosAmount={todosAmount}
+          uncompletedTodosAmount={uncompletedTodosAmount}
+        />
+
+        {!!todosAmount && (
+          <>
+            <TodoList todos={todos} filterOption={filterOption} />
+
+            <Footer
+              filterOption={filterOption}
+              setFilterOption={setFilterOption}
+              todosAmount={todosAmount}
+              uncompletedTodosAmount={uncompletedTodosAmount}
+            />
+          </>
+        )}
+      </div>
+
+      <Notification error={error} onReset={handleResetError} />
+    </div>
   );
 };
